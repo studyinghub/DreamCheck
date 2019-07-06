2a051dd38422133a892f60079e061b5b8dfeeb3f
Raymond Yoo <rayyoo4223@gmail.com>
2019-07-06 13:43:25 -0400

    added the database

--- flutter_app/lib/DreamTitleMakerPage.dart
+++ flutter_app/lib/DreamTitleMakerPage.dart
@@ -3,6 +3,8 @@ import 'package:vision_check_test/step_builder.dart';
 import 'components/Constants.dart';
 import 'category_page.dart';
 import 'home_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class TitleMakerPage extends StatefulWidget {
   static const String id = 'titlemaker_page';
@@ -14,6 +16,27 @@ class _TitleMakerPage extends State<TitleMakerPage> {
   // Create a text controller and use it to retrieve the current value
   // of the TextField.
   final myController = TextEditingController();
+  final _firestore = Firestore.instance;
+  final _auth = FirebaseAuth.instance;
+  FirebaseUser loggedInUser;
+  String titleOfDream;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() async {
+    try {
+      final user = await _auth.currentUser();
+      if (user != null) {
+        loggedInUser = user;
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -60,6 +83,9 @@ class _TitleMakerPage extends State<TitleMakerPage> {
                       color: Colors.black),
                 ),
                 TextField(
+                  onChanged: (value) {
+                    titleOfDream = value;
+                  },
                   textAlign: TextAlign.center,
                   maxLines: 1,
                   style: TextStyle(fontSize: 30.0),
@@ -79,6 +105,12 @@ class _TitleMakerPage extends State<TitleMakerPage> {
                     color: mainButtonColor,
                     textColor: mainButtonTextColor,
                     onPressed: () {
+                      _firestore.collection('dreams').add({
+                        //add the title of the dream with 'email' and
+                        //'title'
+                        'title': titleOfDream,
+                        'email': loggedInUser.email,
+                      });
                       userDreamTitle = myController.text.toString();
                       Navigator.push(
                         context,
